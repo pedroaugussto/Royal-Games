471cf24d88b2264b4434e150d1ffee1038bf8f45
PEDRO AUGUSTO CASTRO GOMES <49078790830@info.local>
2026-02-27 09:10:26 -0300

    :tada: Inicio do projeto e scaffold

---
+++ RoyalGames/Domains/Log_AlteracaoJogo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RoyalGames.Domains;
+
+public partial class Log_AlteracaoJogo
+{
+    public int Log_AlteracaoJogoID { get; set; }
+
+    public DateTime DataAlteracao { get; set; }
+
+    public string? NomeAnterior { get; set; }
+
+    public decimal? PrecoAnterior { get; set; }
+
+    public int? JogoID { get; set; }
+
+    public virtual Jogo? Jogo { get; set; }
+}
